bdc4a621f17acf709bd690a1d44f1d0da919bc3c
Luciogi <lucigithubcommit@skiff.com>
2024-06-29 16:20:04 +0500

    output csv file

--- github.py
+++ github.py
@@ -1,6 +1,7 @@
 import requests as request
 import json
 import time
+import csv
 
 URLS = { "Github": "https://github.com/search"}
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36'}
@@ -17,7 +18,8 @@ profile = text_json["payload"]["results"]
 
 print(f"Page count: {page_count}")
 print(f"Result count: {result_count}")
-
+output_file = open("out.csv", "w")
+csv_writer = csv.writer(output_file)
 
 for page in range(1,page_count+1):
     r = request.get(URLS["Github"], f"q={username_input}&type=users&p={page}", headers=headers)
@@ -28,9 +30,14 @@ for page in range(1,page_count+1):
     else:
         text_json = json.loads(r.text)
         text_json = text_json["payload"]["results"]
-        print(json.dumps(text_json, sort_keys=True, indent=4 ))
-        print(f"==== {page} ====")
+        count = 0
+
+        # Write data in csv file
+        for profile in text_json:
+            if count == 0: # write headers
+                header = profile.keys()
+                csv_writer.writerow(header)
+                count += 1
+            csv_writer.writerow(profile.values())
         time.sleep(2)
-
-
 
